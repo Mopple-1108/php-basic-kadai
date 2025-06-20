635beff6068f534f218fbb2c7c4fbc56fcce623a
MoppleLab <alice.6118@gmail.com>
2025-06-20 19:55:00 +0900

    Update sort.php

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -1,34 +1,40 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <title>PHP基礎編</title>
 </head>
-
 <body>
 <p>
     <?php
-        $nums = array(15, 4, 18, 23, 10);
-
-        $sort_order = isset($_GET['order']) ? $_GET['order'] : 'asc';
-
-        if ($sort_order == 'asc') {
-            echo "昇順にソートします。<br>";
-            sort($nums, SORT_NATURAL | SORT_FLAG_CASE);
+    // 自作のソート関数
+    function sort_2way(&$array, $order) {
+        if ($order == 'asc') {
+            sort($array, SORT_NATURAL | SORT_FLAG_CASE);
         } else {
-            echo "降順にソートします。<br>";
-            rsort($nums, SORT_NATURAL | SORT_FLAG_CASE);
+            rsort($array, SORT_NATURAL | SORT_FLAG_CASE);
         }
+    }
 
-        foreach ($nums as $num) {
-            echo $num . '<br>';
-        }
+    $nums = array(15, 4, 18, 23, 10);
+    $sort_order = isset($_GET['order']) ? $_GET['order'] : 'asc';
+
+    if ($sort_order == 'asc') {
+        echo "昇順にソートします。<br>";
+    } else {
+        echo "降順にソートします。<br>";
+    }
+
+    // 自作のソート関数を呼び出す
+    sort_2way($nums, $sort_order);
+
+    foreach ($nums as $num) {
+        echo $num . '<br>';
+    }
     ?>
 </p>
- <p>
-        <a href="?order=asc">昇順</a> | <a href="?order=desc">降順</a>
+<p>
+    <a href="?order=asc">昇順</a> | <a href="?order=desc">降順</a>
 </p>
 </body>
-
 </html>
