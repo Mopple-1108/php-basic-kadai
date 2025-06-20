669ae85d7c0421650ff6cb7d6a35521c59d116de
MoppleLab <alice.6118@gmail.com>
2025-06-20 13:45:55 +0900

    Update foreach.php

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -8,16 +8,12 @@
 
 <body>
     <p>
-        <?php
-        $personal_date = [
-            ['label' => '名前', 'value' => '玉ねぎ'],
-            ['label' => '値段', 'value' => '200'],
-            ['label' => '産地', 'value' => '北海道']
-        ];
+     <?php
+     $product_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
 
-        foreach ($personal_date as $item) {
-            echo $item['label'] . ': ' . $item['value'] . '<br>';
-        }
+     foreach ($product_data as $key => $value) {
+     echo "{$key} : {$value}<br>";
+    }
         ?>
     </p>
 </body>
